282fbec65f309b7b84fb220fb96b5a185a5520f5
Himanshu <himanshuingle42.com>
2024-08-31 10:30:15 +0530

    Added 3D model and image files, updated App component, and added new styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { Canvas } from "@react-three/fiber";
 import React from "react";
+import "./style.css";
+import { Environment, OrbitControls, ScrollControls } from "@react-three/drei";
+import Mac from "./Mac";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="main h-screen w-full relative">
+      <div className="top-txt font-['Helvetica'] text-center w-1/3 text-white absolute top-[6.5rem] left-1/2 -translate-x-1/2">
+        <h1 className="text-5xl text-gradient font-bold tracking-tighter">macbook pro.</h1>
+        <h1>oh so pro!</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+          quod ducimus aut!
+        </p>
+      </div>
+      <Canvas camera={{ fov: 12, position: [0, -10, 220] }}>
+        <Environment
+          files={
+            "https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_09_4k.exr"
+          }
+        />
+        <ScrollControls>
+          <Mac />
+        </ScrollControls>
+      </Canvas>
+    </div>
+  );
 };
 
 export default App;
